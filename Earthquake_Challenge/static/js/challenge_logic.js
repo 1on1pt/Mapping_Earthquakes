d3c33596d04dd1a50ace365a25d9cae44f0b7f03
John Beauchamp <94148420+1on1pt@users.noreply.github.com>
2022-03-19 15:55:56 -0500

    Updating challenge_logic.js file.

--- Earthquake_Challenge/static/js/challenge_logic.js
+++ Earthquake_Challenge/static/js/challenge_logic.js
@@ -30,10 +30,12 @@ let baseMaps = {
 
 // 1. Add a 2nd layer group for the tectonic plate data.
 let allEarthquakes = new L.LayerGroup();
+let tectonicPlates = new L.LayerGroup();
 
 
 // 2. Add a reference to the tectonic plates group to the overlays object.
 let overlays = {
+  "Tectonic Plates": tectonicPlates,
   "Earthquakes": allEarthquakes
 };
 
@@ -141,7 +143,17 @@ legend.onAdd = function() {
 
 
   // 3. Use d3.json to make a call to get our Tectonic Plate geoJSON data.
-  d3.json().then(() {
+  let tectData = "https://raw.githubusercontent.com/fraxen/tectonicplates/master/GeoJSON/PB2002_boundaries.json";
+
+  d3.json(tectData).then(function(data) {
+    console.log(data);
     
+    L.geoJson(data, {
+      color: "blue",
+      weight: 3
+
+    }).addTo(tectonicPlates);
+    
+  tectonicPlates.addTo(map);
   });
 });
