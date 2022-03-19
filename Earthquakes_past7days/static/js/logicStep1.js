422474b6b0359adb77fad1e0552428a0bc802492
John Beauchamp <94148420+1on1pt@users.noreply.github.com>
2022-03-19 10:36:17 -0500

    Updating index.html and logicStep1.js files.

--- Earthquakes_past7days/static/js/logicStep1.js
+++ Earthquakes_past7days/static/js/logicStep1.js
@@ -18,13 +18,13 @@ attribution: 'Map data © <a href="https://www.openstreetmap.org/">OpenStreetMap
 // Create a base layer that holds both maps.
 let baseMaps = {
   'Streets': streets,
-  'Satellite Streets': satelliteStreets
+  'Satellite': satelliteStreets
 };
 
 // Create the map object with center, zoom level and default layer.
 let map = L.map('mapid', {
-  center: [43.7, -79.3],
-  zoom: 11,
+  center: [39.5, -98.5],
+  zoom: 3,
   layers: [streets]
 });
 
@@ -32,24 +32,16 @@ let map = L.map('mapid', {
 L.control.layers(baseMaps).addTo(map);
 
 // Accessing the Toronto airline routes GeoJSON URL
-let torontoHoods = "https://raw.githubusercontent.com/1on1pt/Mapping_Earthquakes/main/torontoNeighborhoods.json";
+// let torontoHoods = "https://raw.githubusercontent.com/1on1pt/Mapping_Earthquakes/main/torontoNeighborhoods.json";
 
-// Create style for lines and fill.
-let myStyle = {
-  fillColor: "#ffffa1",
-  color: "blue",
-  weight: 1
-}
-// Grabbing our GeoJSON data.
-d3.json(torontoHoods).then(function(data) {
-  console.log(data);
-// Creating a GeoJSON layer with the retrieved data.
-L.geoJSON(data, {
-  style: myStyle,
-  onEachFeature: function(feature,layer){
-    console.log(layer);
-    layer.bindPopup("<h4> Neighborhood: " + feature.properties.AREA_NAME + "</h4>");
-  }
-}).addTo(map);
-
+// // Create style for lines and fill.
+// let myStyle = {
+//   fillColor: "#ffffa1",
+//   color: "blue",
+//   weight: 1
+// }
+// Retrieve the earthquake GeoJSON data.
+d3.json("https://earthquake.usgs.gov/earthquakes/feed/v1.0/summary/all_week.geojson").then(function(data) {
+  // Creating a GeoJSON layer with the retrieved data.
+  L.geoJSON(data).addTo(map);
 });
